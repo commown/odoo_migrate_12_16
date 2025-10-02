077a3fe3dbc750b1bfea88c9e5f27fe69e35a7f0
Florent Cayré <florent@commown.coop>
2025-10-02 14:27:18 +0200

    [pre-14.0] Avoid a missing payment method on account payment crash
    ... by inserting an inbound payment method for the paypal journal.
    The concerned account payments were tests, but we can't remove them because
    their number is supposed to be continuous.

--- scripts/step_03__openupgrade__14.0/pre-migration.sql
+++ scripts/step_03__openupgrade__14.0/pre-migration.sql
@@ -1,1 +1,11 @@
+-- Add a inbound payment method for journal "Règlements Fournisseurs -
+-- Paypal" because there are test account payment (IDS: 2352, 2355,
+-- 2357, 2356, 2358, 2360, 2362, 2359, 2361, 2365, 2364, 2363) that
+-- are inbound but the journal does not support it.
 
+-- We cannot simply remove these payments because their number are
+-- supposed to be continuous.
+
+INSERT INTO account_journal_inbound_payment_method_rel
+VALUES (11, 1)
+ON CONFLICT DO NOTHING;
