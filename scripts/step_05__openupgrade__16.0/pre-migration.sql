dfbb2e9ded8532fe4fc46a75217272e3d1fa26c6
Florent Cayré <florent@commown.coop>
2025-10-06 16:51:27 +0200

    WIP 16.0 migration scripts
    - impact of module splits: xml id renames
    - old views removal which block the migration

--- scripts/step_05__openupgrade__16.0/pre-migration.sql
+++ scripts/step_05__openupgrade__16.0/pre-migration.sql
@@ -1,1 +1,55 @@
+-- Ticket #42292
+UPDATE ir_model_data SET module='customer_manager_base'
+WHERE module='customer_team_manager' AND name='group_customer_admin';
 
+-- Ticket #40613
+UPDATE ir_model_data SET module='commown_investment_sale'
+WHERE module='commown' AND name='investment_payment_term';
+
+UPDATE ir_model_data SET module='commown_investment_sale'
+WHERE module='commown' AND name='investment_followup_project';
+
+UPDATE ir_model_data SET module='commown_investment_sale'
+WHERE module='commown' AND name='investment_followup_start_stage';
+
+-- Ticket #40616
+UPDATE ir_model_data SET module='commown_support'
+WHERE module='commown' AND NAME= 'mail_template_issue_reminder';
+
+UPDATE ir_model_data SET module='commown_support'
+WHERE module='commown' AND NAME= 'sms_template_issue_reminder';
+
+UPDATE ir_model_data SET module='commown_support'
+WHERE module='commown_self_troubleshooting' AND NAME= 'support_project';
+
+UPDATE ir_model_data SET module='commown_support'
+WHERE module='commown_self_troubleshooting' AND NAME= 'commercial_project';
+
+UPDATE ir_model_data SET module='commown_support'
+WHERE module='commown_self_troubleshooting' AND NAME= 'stage_callback';
+
+UPDATE ir_model_data SET module='commown_support'
+WHERE module='commown_self_troubleshooting' AND NAME= 'stage_pending';
+
+UPDATE ir_model_data SET module='commown_support'
+WHERE module='commown_self_troubleshooting' AND NAME= 'stage_received';
+
+UPDATE ir_model_data SET module='commown_support'
+WHERE module='commown_self_troubleshooting' AND NAME= 'stage_solved';
+
+UPDATE ir_model_data SET module='commown_support'
+WHERE module='commown_self_troubleshooting' AND NAME= 'stage_long_term_followup';
+
+-- Ticket #40659
+UPDATE ir_model_data SET module='website_b2b'
+WHERE module='website_sale_b2b' AND NAME= 'b2b_website';
+
+-- Ticket #40659
+DELETE from ir_model_data
+WHERE module='commown' and name='mass_reconcile';
+
+DELETE from ir_model_data
+WHERE module='commown' and name='reconcile_method_simple_partner_custom';
+
+-- Ticket #40337
+DELETE from ir_actions WHERE id=460;
--- scripts/step_06__regular__16.0/pre-migration.sql
+++ scripts/step_06__regular__16.0/pre-migration.sql
@@ -1,1 +1,3 @@
-
+delete from ir_ui_view where name='commown_automated_control.base_automation';
+delete from ir_ui_view where name='crm.phone.preferences.option.view';
+delete from ir_ui_view where name='crm.phone.res.users.form';
