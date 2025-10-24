81500404c91759db888dd42bf06d6b8dad027d6f
Lena MONTENOT <lena.montenot@gmail.com>
2025-10-24 16:37:38 +0200

    [16.0] Add tech_name fields to records using env. server variables
    See task #43651

--- scripts/step_06__regular__16.0/post-migration.py
+++ scripts/step_06__regular__16.0/post-migration.py
@@ -7,4 +7,9 @@ env = env  # noqa: F821
 
 # Write custom script here
 
+slimpay_tech_name = ""
+env.ref("account_payment_slimpay.payment_provider_slimpay").tech_name = "payment_provider_slimpay"
+
+colissimo 
+
 env.cr.commit()
