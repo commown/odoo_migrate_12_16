cfe39ca2beb434c4b98f13d41172a331ca85c328
Lena MONTENOT <lena.montenot@gmail.com>
2025-10-30 11:48:42 +0100

    [migrate] Added SQL/Python commands to set Captcha values and move Hcaptcha keys

--- scripts/step_06__regular__16.0/post-migration.py
+++ scripts/step_06__regular__16.0/post-migration.py
@@ -7,6 +7,18 @@ env = env  # noqa: F821
 
 # Write custom script here
 
+# Ticket 39426
+env.ref("website.default_website").update(
+    {
+        "recaptcha_v2_enabled": True,
+        "recaptcha_v2_resp_attr": "h-captcha-response",
+        "recaptcha_v2_html_class": "h-captcha",
+        "recaptcha_v2_api_url": "https://js.hcaptcha.com/1/api.js",
+        "recaptcha_v2_verify_url": "https://api.hcaptcha.com/siteverify",
+    }
+)
+
+
 # cf. PR 481: Assign tech_name to noupdate records
 
 env.ref("account_payment_slimpay.payment_provider_slimpay").tech_name = "payment_provider_slimpay"
