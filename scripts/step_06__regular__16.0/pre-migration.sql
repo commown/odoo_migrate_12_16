20adf25b2ff509b953f3630060be11e9a40281f5
Florent Cayré <florent@commown.coop>
2025-11-27 14:37:17 +0100

    Move tech_name field write from post-migration.py to pre-migration.py

--- scripts/step_06__regular__16.0/pre-migration.sql
+++ scripts/step_06__regular__16.0/pre-migration.sql
@@ -8,3 +8,11 @@ update mail_channel C set channel_type='channel'
 where exists(
     select 1 from res_partner P where P.mail_channel_id = C.id
 );
+
+-- Ticket #43657/#
+update payment_provider set tech_name = 'slimpay' where id = 11; -- account_payment_slimpay.payment_provider_slimpay
+update commown_shipping_account set tech_name = 'shipping-account-colissimo-std-account'
+where id = 1; -- commown_shipping.shipping-account-colissimo-std-account
+update commown_shipping_account set tech_name = 'shipping-account-colissimo-support-account'
+where id = 2; -- commown_shipping.shipping-account-colissimo-support-account
+
