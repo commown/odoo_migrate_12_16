210d1943d9be25d28fb1905e441831c9c409bbf0
Florent Cayré <florent@commown.coop>
2025-11-28 09:13:00 +0100

    [15.0] Added web_chatter_position installation to run migration script in v16
    (see related task for more info)

--- scripts/step_04__openupgrade__15.0/post-migration.py
+++ scripts/step_04__openupgrade__15.0/post-migration.py
@@ -5,6 +5,8 @@ _logger.info("Executing post-migration.py script ...")
 
 env = env  # noqa: F821
 
-# Write custom script here
+# Ticket #44870
+# -> Install this module now to trigger its v16 post-migration script
+env["ir.module.module"].search([("name", "=", "web_chatter_position")]).button_immediate_install()
 
 env.cr.commit()
