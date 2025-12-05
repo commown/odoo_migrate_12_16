b8b0a2c98e1b695b7f657863693128e88dc5e6c0
Florent Cayré <florent@commown.coop>
2025-12-05 12:37:49 +0100

    [step 4 (14->15)] Added request to assign newly created payment method lines to unassigned account payments
    This query is taken from the account 15.0 end-migration.py script, which assigns an account.payment.method.line id
    to account.payment records.
    
    We execute it again since our account.payment.method.line records are created in the payment end-migration script,
    which is only executed after the account script, and as such, the newly created lines are not assigned.
    
    (See ticket #44773 for more info)

--- scripts/step_04__openupgrade__15.0/post-migration.py
+++ scripts/step_04__openupgrade__15.0/post-migration.py
@@ -9,4 +9,16 @@ env = env  # noqa: F821
 # -> Install this module now to trigger its v16 post-migration script
 env["ir.module.module"].search([("name", "=", "web_chatter_position")]).button_immediate_install()
 
+# Ticket #44773
+# Re-execute end-migration account request, to assign newly created payment method lines to older account.payment records
+env.cr.execute(
+    """
+    UPDATE account_payment ap
+    SET payment_method_line_id = apml.id
+    FROM account_move am
+    JOIN account_payment_method_line apml ON apml.journal_id = am.journal_id
+    WHERE ap.move_id = am.id AND ap.payment_method_id = apml.payment_method_id
+    """
+)
+
 env.cr.commit()
