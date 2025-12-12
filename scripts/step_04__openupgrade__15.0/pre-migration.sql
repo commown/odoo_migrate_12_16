39d98141add1cbfc2eeaf929d93dd293e7107792
Florent Cayré <florent@commown.coop>
2025-12-12 10:08:58 +0100

    [step 4(14->15)] Updated payment method names on Slimpay methods

--- scripts/step_04__openupgrade__15.0/pre-migration.sql
+++ scripts/step_04__openupgrade__15.0/pre-migration.sql
@@ -1,4 +1,7 @@
 -- Ticket #44773/#
 update account_payment_method
-set code = 'slimpay' where code = 'electronic';
+set
+    code = 'slimpay',
+    name = 'Slimpay (' || payment_type || ')'
+where code = 'electronic';
 
