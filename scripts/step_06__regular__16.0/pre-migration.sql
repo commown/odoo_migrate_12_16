7c407642827ead39277a0e5552a107429a3bd2da
Lena MONTENOT <lena.montenot@gmail.com>
2026-01-29 10:43:25 +0100

    [step 6 (16.0 final.)] Removed mail_channel type conversion from group to channel
    Due to the several disadventages of channel-type mail.channels for B2B channels (ie. B2B users being able to see
    other B2B channels publicly, being unable to restrict the channel to each company without creating one group per company,
    unrestricted pings), we decided to leave channels as their current type, meaning group.
    
    Also, while the group channels take a while to load, channel types don't feel much quicker to load, according to users.
    
    See ticket #46981

--- scripts/step_06__regular__16.0/pre-migration.sql
+++ scripts/step_06__regular__16.0/pre-migration.sql
@@ -2,13 +2,6 @@ delete from ir_ui_view where name='commown_automated_control.base_automation';
 delete from ir_ui_view where name='crm.phone.preferences.option.view';
 delete from ir_ui_view where name='crm.phone.res.users.form';
 
--- Ticket #43988/#
--- Written by Florent Cayre
-update mail_channel C set channel_type='channel'
-where exists(
-    select 1 from res_partner P where P.mail_channel_id = C.id
-);
-
 -- Ticket #43657/#
 update payment_provider set tech_name = 'slimpay' where id = 11; -- account_payment_slimpay.payment_provider_slimpay
 update commown_shipping_account set tech_name = 'shipping-account-colissimo-std-account'
