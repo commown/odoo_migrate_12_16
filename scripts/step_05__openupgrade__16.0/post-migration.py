85ed0d66a292a4074fde8a44a0127b6a93ad8bd2
Florent Cayré <florent@commown.coop>
2026-02-26 13:34:40 +0100

    [step 5 (15.0->16.0)] Set unwanted im_livechat form view with a low priority
    This view is a simplified view of the im_livechat.channel model, meant to be used when creating
    a livechat channel through a create action.
    
    However, since it has currently by default a higher priority than other views, we set it low manually.
    
    We do it in step 5, so that the view priority is taken into account after website_livechat being updated during step 6.

--- scripts/step_05__openupgrade__16.0/post-migration.py
+++ scripts/step_05__openupgrade__16.0/post-migration.py
@@ -5,6 +5,9 @@ _logger.info("Executing post-migration.py script ...")
 
 env = env  # noqa: F821
 
-# Write custom script here
+# Ticket #47933
+# Setting this view's priority low, so that after the next update,
+# it doesn't take priority over other views.
+env.ref("website_livechat.im_livechat_channel_view_form_add").priority = 99
 
 env.cr.commit()
